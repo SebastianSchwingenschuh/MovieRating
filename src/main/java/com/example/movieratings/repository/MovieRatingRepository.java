2d7e7338f866fb9019f607284f386bbf9fae90c7
SEbastianSchwingenschuh <sebastianschwingii@gmail.com>
2026-06-10 11:33:03 +0200

    mit repo angefangen

--- src/main/java/com/example/movieratings/repository/MovieRatingRepository.java
+++ src/main/java/com/example/movieratings/repository/MovieRatingRepository.java
@@ -1,4 +1,19 @@
 package com.example.movieratings.repository;
 
+import com.example.movieratings.model.MovieRating;
+
 public class MovieRatingRepository {
+    MovieRatingRepository instance;
+
+    public MovieRatingRepository() {
+    }
+
+    public MovieRatingRepository getInstance(){
+        if(instance == null){
+            instance = new MovieRatingRepository();
+        }
+        return instance;
+    }
+
+
 }

a4a3caef7bf8b31f90638a49be70ae036e51da59
SebastianSchwingenschuh <sebastianschwingii@gmail.com>
2026-06-13 16:25:34 +0200

    model mit Simple Propertys gemacht

--- src/main/java/com/example/movieratings/model/MovieRating.java
+++ src/main/java/com/example/movieratings/model/MovieRating.java
@@ -1,11 +1,15 @@
 package com.example.movieratings.model;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.Objects;
 
 public class MovieRating {
-    private String title;
-    private int year;
-    private double rating;
+    private SimpleStringProperty title;
+    private SimpleIntegerProperty year;
+    private SimpleDoubleProperty rating;
 
     public MovieRating(String title, int year, double rating) {
         this.setTitle(title);
@@ -14,36 +18,47 @@ public class MovieRating {
     }
 
     public String getTitle() {
+        return title.get();
+    }
+
+    public SimpleStringProperty titleProperty() {
         return title;
     }
 
-    private void setTitle(String title) {
+    public void setTitle(String title) {
         if(title == null || title.isEmpty()){
             throw new IllegalArgumentException("title must not be null or empty");
         }
-        this.title = title;
+        this.title.set(title);
     }
 
     public int getYear() {
+        return year.get();
+    }
+
+    public SimpleIntegerProperty yearProperty() {
         return year;
     }
 
-    private void setYear(int year) {
-        if(year <= 1888){
+    public void setYear(int year) {if(year <= 1888){
             throw new IllegalArgumentException("there is no older film than 1888");
         }
-        this.year = year;
+        this.year.set(year);
     }
 
     public double getRating() {
+        return rating.get();
+    }
+
+    public SimpleDoubleProperty ratingProperty() {
         return rating;
     }
 
-    private void setRating(double rating) {
+    public void setRating(double rating) {
         if(rating < 1.0 || rating > 5.0){
             throw new IllegalArgumentException("rating must be between 1.0 and 5.0");
         }
-        this.rating = rating;
+        this.rating.set(rating);
     }
 
     @Override
